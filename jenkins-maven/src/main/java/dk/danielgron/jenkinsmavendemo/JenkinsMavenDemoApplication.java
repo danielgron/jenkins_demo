ce88ee0fd8ed2561700989f8c478a3aec81f1335
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2021-11-23 21:21:40 +0100

    add spring project

---
+++ jenkins-maven/src/main/java/dk/danielgron/jenkinsmavendemo/JenkinsMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package dk.danielgron.jenkinsmavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsMavenDemoApplication.class, args);
+    }
+
+}
---
+++ jenkins-maven/src/test/java/dk/danielgron/jenkinsmavendemo/JenkinsMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.danielgron.jenkinsmavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMavenDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
